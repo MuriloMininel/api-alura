c3e4f09021187064800d56b72a4d46a64a9d81d5
Murilo <murilomininel1@hotmail.com>
2021-11-23 21:03:37 -0300

    criando conexao com a database

--- infra/conexao.js
+++ infra/conexao.js
@@ -3,7 +3,7 @@ const mysql = require('mysql')
 const conexao = mysql.createConnection({
 
     host: 'localhost',
-    port: 3306,
+    port: '3306',
     user: 'root',
     password: 'admin',
     database: 'api-alura'
